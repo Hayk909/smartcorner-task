5a6e38db2ac2aeea9ba9e64e171c4a67373fb635
Hayk909 <hayk_ohanyan2000@mail.ru>
2022-02-04 23:59:21 +0400

    Set to https

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: `http://${process.env.HOST}:${process.env.SERVER_PORT}/api`,
+    baseURL: `https://${process.env.HOST}:${process.env.SERVER_PORT}/api`,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
